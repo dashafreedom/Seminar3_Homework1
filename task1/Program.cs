ca496557db0ae5f54a15797d8a6d4679783471b6
DaryaLukyanova <dasha.opt@mail.ru>
2022-11-22 01:19:48 +0500

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,16 @@
+﻿string number = ReadInt("Введите пятизначное число: ");
+
+if(number[0] == number[4] && number[1] == number[3])
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+
+string ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToString(Console.ReadLine());
+}
