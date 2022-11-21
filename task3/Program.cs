6dde9432334a30318aa34f2190d5239a25cb05bb
DaryaLukyanova <dasha.opt@mail.ru>
2022-11-22 01:04:11 +0500

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,12 @@
+﻿int number = ReadInt("Введите число: ");
+int i = 1;
+while(i <= number)
+{
+    Console.Write(i * i * i++ + " ");
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
